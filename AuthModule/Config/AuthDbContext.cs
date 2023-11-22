bf879b839024844b000fb6d4e712c48cd40387e0
BrunoPerkovic <brunko10@gmail.com>
2023-11-22 12:42:00 +0100

    first code

---
+++ AuthModule/Config/AuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AuthModule.BL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthModule.Config;
+
+public class AuthDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DićemoDatabase");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
---
+++ AuthModule/Program.cs
@@ -0,0 +1,34 @@
+using AuthModule.BL.Interfaces;
+using AuthModule.BL.Services;
+using AuthModule.Config;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Services implementation
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddDbContext<AuthDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DićemoDatabase")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
